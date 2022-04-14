dfd7dab5e25da499c6f71be3fd568788afcdf216
mixei4ik <mixei4ik87@gmail.com>
2022-04-14 12:19:19 +0300

    add data and Api.kt

---
+++ app/src/main/java/com/example/a22bytetesttask/Api.kt
@@ -0,0 +1,43 @@
+package com.example.a22bytetesttask
+
+import com.example.a22bytetesttask.data.ApiData
+import com.example.a22bytetesttask.data.News
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface NewsApi {
+    @GET("/v2/everything?q=Apple&from=2022-04-14&sortBy=popularity&apiKey=9cdb1537a38b48cc8f7074a56c66d434")
+    suspend fun getListOfNews(): ApiData
+}
+
+object NewsApiImpl {
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("https://newsapi.org")
+        .build()
+
+    private val newsApiService = retrofit.create(NewsApi::class.java)
+
+    suspend fun getListOfNews(): List<News> {
+        return withContext(Dispatchers.IO) {
+            var id = 0
+            newsApiService.getListOfNews()
+                .articles
+                .map { article ->
+                    News(
+                        id = id++,
+                        author = article.author ?: "not author",
+                        content = article.content ?: "not content",
+                        description = article.description ?: "not description",
+                        publishedAt = article.publishedAt ?: "unknown",
+                        title = article.title ?: "Title",
+                        url = article.url ?: "not url",
+                        urlToImage = article.urlToImage ?: ""
+                    )
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a22bytetesttask/data/News.kt
@@ -0,0 +1,12 @@
+package com.example.a22bytetesttask.data
+
+data class News(
+    val id: Int,
+    val author: String,
+    val content: String,
+    val description: String,
+    val publishedAt: String,
+    val title: String,
+    val url: String,
+    val urlToImage: String
+)
